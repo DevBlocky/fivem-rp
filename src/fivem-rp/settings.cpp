ed2d33d48495d37563e97e0c6e12e3c60a79bf93
BlockBa5her <blocckba5her@gmail.com>
2018-03-11 14:00:26 -0700

    remove brackets and remove try/catch

--- src/fivem-rp/settings.cpp
+++ src/fivem-rp/settings.cpp
@@ -10,63 +10,42 @@ unique_ptr<settings> s;
 
 void reload_settings(const std::string &path)
 {
-	try
-	{
-		Json::Value json;
-		ifstream stream(path, ifstream::binary);
+	Json::Value json;
+	ifstream stream(path, ifstream::binary);
+	if (stream.good())
 		stream >> json;
 
-		s = make_unique<settings>();
+	s = make_unique<settings>();
 
-		if (json.isNull())
-		{
-			s->problem = "file_not_read";
-			return;
-		}
-
-		const Json::Value features = json.get("feature-enable", 0);
-		if (!features.isInt())
-		{
-			if (features.isObject())
-			{
-				for (const auto &member : features.getMemberNames())
-					s->enabled_features.insert(pair<string, bool>(member, features.get(member, false).asBool()));
-			}
-			else
-			{
-				s->problem = "feature_not_object";
-			}
-		}
-		else
-		{
-			s->problem = "features_null";
-		}
-
-		const Json::Value keys = json.get("keys", 0);
-		if (!keys.isInt())
-		{
-			if (keys.isObject())
-			{
-				for (const auto &member : keys.getMemberNames())
-					s->keys.insert(pair<string, int>(member, keys.get(member, 0).asInt()));
-			}
-			else
-			{
-				s->problem = "keys_not_object";
-			}
-		}
-		else
-		{
-			s->problem = "keys_null";
-		}
-	}
-	catch (std::exception &e) 
+	if (json.isNull())
 	{
-		s->enabled_features.clear();
-		s->keys.clear();
-
-		s->problem = e.what();
+		s->problem = "file_not_read";
+		return;
 	}
+
+	const Json::Value features = json.get("feature-enable", Json::Value());
+	if (!features.isNull())
+	{
+		if (features.isObject())
+			for (const auto &member : features.getMemberNames())
+				s->enabled_features.insert(pair<string, bool>(member, features.get(member, false).asBool()));
+		else
+			s->problem = "feature_not_object";
+	}
+	else
+		s->problem = "features_null";
+
+	const Json::Value keys = json.get("keys", Json::Value());
+	if (!keys.isNull())
+	{
+		if (keys.isObject())
+			for (const auto &member : keys.getMemberNames())
+				s->keys.insert(pair<string, int>(member, keys.get(member, 0).asInt()));
+		else
+			s->problem = "keys_not_object";
+	}
+	else
+		s->problem = "keys_null";
 }
 
 settings& get_settings()

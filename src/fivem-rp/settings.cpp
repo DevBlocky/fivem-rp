caeb42106c8f51eee0bd561075edda098d83ecab
BlockBa5her <blocckba5her@gmail.com>
2018-03-12 17:46:22 -0700

    using new json library

--- src/fivem-rp/settings.cpp
+++ src/fivem-rp/settings.cpp
@@ -1,62 +1,60 @@
 #include "settings.h"
-#include <jsoncpp/json/json.h>
+#include <json/json.hpp>
 #include <fstream>
-#include <numeric>
 #include <memory>
 
 using namespace std;
+using json_value = nlohmann::basic_json<>;
 
 unique_ptr<settings> s;
 
 void reload_settings(const std::string &path)
 {
-	Json::Value json;
+	json_value json;
 	ifstream stream(path, ifstream::binary);
 	if (stream.good())
 		stream >> json;
 
 	s = make_unique<settings>();
 
-	if (json.isNull())
+	if (json.is_null())
 	{
 		s->problem = "file_not_read";
 		return;
 	}
 
-	const Json::Value features = json.get("feature-enable", Json::Value());
-	if (!features.isNull())
+	json_value features = json["feature-enable"];
+	if (!features.is_null())
 	{
-		if (features.isObject())
-			for (const auto &member : features.getMemberNames())
-				s->enabled_features.insert(pair<string, bool>(member, features.get(member, false).asBool()));
+		if (features.is_object())
+			for (auto it = features.begin(); it != features.end(); ++it)
+				s->enabled_features.insert(pair<string, bool>(it.key(), it.value()));
 		else
 			s->problem = "feature_not_object";
 	}
 	else
 		s->problem = "features_null";
 
-	const Json::Value keys = json.get("keys", Json::Value());
-	if (!keys.isNull())
+	const json_value keys = json["keys"];
+	if (!keys.is_null())
 	{
-		if (keys.isObject())
-			for (const auto &member : keys.getMemberNames())
-				s->keys.insert(pair<string, int>(member, keys.get(member, 0).asInt()));
+		if (keys.is_object())
+			for (auto it = keys.begin(); it != keys.end(); ++it)
+				s->keys.insert(pair<string, int>(it.key(), it.value()));
 		else
 			s->problem = "keys_not_object";
 	}
 	else
 		s->problem = "keys_null";
 
-	const Json::Value m = json.get("measurement", Json::Value());
-	if (!m.isNull())
+	const json_value m = json["measurement"];
+	if (!m.is_null())
 	{
-		if (m.isString())
+		if (m.is_string())
 		{
-			const string str = m.asString();
+			const string str = m;
 			if (str != "imperial" && str != "metric")
-			{
 				s->problem = "measurement_not_measurement";
-			}
 			else
 				s->measurement = str;
 		}
